858add1122298e33051ee476cccef8f578be9b77
Ben Wesch <benjamin.wesch@gmail.com>
2024-09-16 15:36:43 +0200

    zero-fill instead of wrap for missing input channels

--- qacc~.c
+++ qacc~.c
@@ -25,9 +25,11 @@ static t_class *qacc_tilde_class;
 typedef struct _qacc_tilde {
     t_object  x_obj;
     t_sample f;
-    t_sample **quat_vec;   // 4-channel quaternion input
-    t_sample **out_vec;    // 4-channel quaternion output
+    t_sample **quat_in;   // 4-channel quaternion input
+    t_sample **quat_out;    // 4-channel quaternion output
     t_float accum_quat[4]; // Accumulated quaternion state
+    t_sample *zero_buffer; // Dynamic zero buffer
+    int buffer_size;       // Size of the zero buffer
 } t_qacc_tilde;
 
 t_int *qacc_tilde_perform(t_int *w)
@@ -35,8 +37,8 @@ t_int *qacc_tilde_perform(t_int *w)
     t_qacc_tilde *x = (t_qacc_tilde *)(w[1]);
     int n = (int)(w[2]);
 
-    t_sample *qw = x->quat_vec[0], *qx = x->quat_vec[1], *qy = x->quat_vec[2], *qz = x->quat_vec[3];
-    t_sample *ow = x->out_vec[0], *ox = x->out_vec[1], *oy = x->out_vec[2], *oz = x->out_vec[3];
+    t_sample *qw = x->quat_in[0], *qx = x->quat_in[1], *qy = x->quat_in[2], *qz = x->quat_in[3];
+    t_sample *ow = x->quat_out[0], *ox = x->quat_out[1], *oy = x->quat_out[2], *oz = x->quat_out[3];
 
     t_float aw = x->accum_quat[0], ax = x->accum_quat[1], ay = x->accum_quat[2], az = x->accum_quat[3];
 
@@ -75,14 +77,36 @@ t_int *qacc_tilde_perform(t_int *w)
 void qacc_tilde_dsp(t_qacc_tilde *x, t_signal **sp)
 {
     int quat_channels = (int)sp[0]->s_nchans;
+    int vec_size = sp[0]->s_n;
 
-    // wrap quaternion channels if less than 4 given
-    for (int i = 0; i < 4; i++)
-        x->quat_vec[i] = sp[0]->s_vec + sp[0]->s_n * (i % quat_channels);
-    
+    // Reallocate zero buffer if necessary
+    if (x->buffer_size != vec_size) {
+        if (x->zero_buffer) {
+            freebytes(x->zero_buffer, x->buffer_size * sizeof(t_sample));
+        }
+        x->zero_buffer = (t_sample *)getbytes(vec_size * sizeof(t_sample));
+        if (!x->zero_buffer) {
+            pd_error(x, "qacc~: out of memory");
+            return;
+        }
+        x->buffer_size = vec_size;
+        for (int i = 0; i < vec_size; i++) {
+            x->zero_buffer[i] = 0;
+        }
+    }
+
+    // Assign input channels or zero buffer
+    for (int i = 0; i < 4; i++) {
+        if (i < quat_channels) {
+            x->quat_in[i] = sp[0]->s_vec + vec_size * i;
+        } else {
+            x->quat_in[i] = x->zero_buffer;
+        }
+    }
+
     signal_setmultiout(&sp[1], 4);
     for (int i = 0; i < 4; i++)
-        x->out_vec[i] = sp[1]->s_vec + sp[1]->s_n * i;
+        x->quat_out[i] = sp[1]->s_vec + sp[1]->s_n * i;
 
     dsp_add(qacc_tilde_perform, 2, x, sp[0]->s_n);
 }
@@ -104,14 +128,18 @@ void *qacc_tilde_new(void)
 {
     t_qacc_tilde *x = (t_qacc_tilde *)pd_new(qacc_tilde_class);
 
-    x->quat_vec = (t_sample **)getbytes(4 * sizeof(t_sample *));
-    x->out_vec = (t_sample **)getbytes(4 * sizeof(t_sample *));
+    x->quat_in = (t_sample **)getbytes(4 * sizeof(t_sample *));
+    x->quat_out = (t_sample **)getbytes(4 * sizeof(t_sample *));
 
     // initialize accumulated quaternion to identity
     x->accum_quat[0] = 1;
     x->accum_quat[1] = 0;
     x->accum_quat[2] = 0;
     x->accum_quat[3] = 0;
+
+    // Initialize zero buffer
+    x->zero_buffer = NULL;
+    x->buffer_size = 0;
 
     outlet_new(&x->x_obj, &s_signal);
 
@@ -120,8 +148,11 @@ void *qacc_tilde_new(void)
 
 void qacc_tilde_free(t_qacc_tilde *x)
 {
-    freebytes(x->quat_vec, 4 * sizeof(t_sample *));
-    freebytes(x->out_vec, 4 * sizeof(t_sample *));
+    freebytes(x->quat_in, 4 * sizeof(t_sample *));
+    freebytes(x->quat_out, 4 * sizeof(t_sample *));
+    if (x->zero_buffer) {
+        freebytes(x->zero_buffer, x->buffer_size * sizeof(t_sample));
+    }
 }
 
 void qacc_tilde_setup(void)
--- qvrot~.c
+++ qvrot~.c
@@ -19,8 +19,10 @@ typedef struct _qvrot_tilde {
     t_object  x_obj;
     t_sample f;
     t_sample **vec_in;   // 3-channel vector input
-    t_sample **quat_vec; // 4-channel quaternion input
-    t_sample **out_vec;  // 3-channel vector output
+    t_sample **quat_in; // 4-channel quaternion input
+    t_sample **vec_out;  // 3-channel vector output
+    t_sample *zero_buffer; // Dynamic zero buffer
+    int buffer_size;       // Size of the zero buffer
 } t_qvrot_tilde;
 
 t_int *qvrot_tilde_perform(t_int *w)
@@ -28,8 +30,8 @@ t_int *qvrot_tilde_perform(t_int *w)
     t_qvrot_tilde *x = (t_qvrot_tilde *)(w[1]);
     int n = (int)(w[2]);
     t_sample *vx = x->vec_in[0], *vy = x->vec_in[1], *vz = x->vec_in[2];
-    t_sample *qw = x->quat_vec[0], *qx = x->quat_vec[1], *qy = x->quat_vec[2], *qz = x->quat_vec[3];
-    t_sample *ox = x->out_vec[0], *oy = x->out_vec[1], *oz = x->out_vec[2];
+    t_sample *qw = x->quat_in[0], *qx = x->quat_in[1], *qy = x->quat_in[2], *qz = x->quat_in[3];
+    t_sample *ox = x->vec_out[0], *oy = x->vec_out[1], *oz = x->vec_out[2];
 
     while (n--) {
         t_float rx = (*vx)*((*qw)*(*qw) + (*qx)*(*qx) - (*qy)*(*qy) - (*qz)*(*qz)) + 
@@ -56,18 +58,44 @@ void qvrot_tilde_dsp(t_qvrot_tilde *x, t_signal **sp)
 {
     int vec_channels = (int)sp[0]->s_nchans;
     int quat_channels = (int)sp[1]->s_nchans;
+    int vec_size = sp[0]->s_n;
 
-    // wrap vector channels if less than 3 given
-    for (int i = 0; i < 3; i++)
-        x->vec_in[i] = sp[0]->s_vec + sp[0]->s_n * (i % vec_channels);
+    // Reallocate zero buffer if necessary
+    if (x->buffer_size != vec_size) {
+        if (x->zero_buffer) {
+            freebytes(x->zero_buffer, x->buffer_size * sizeof(t_sample));
+        }
+        x->zero_buffer = (t_sample *)getbytes(vec_size * sizeof(t_sample));
+        if (!x->zero_buffer) {
+            pd_error(x, "qvrot~: out of memory");
+            return;
+        }
+        x->buffer_size = vec_size;
+        for (int i = 0; i < vec_size; i++) {
+            x->zero_buffer[i] = 0;
+        }
+    }
 
-    // wrap quaternion channels if less than 4 given
-    for (int i = 0; i < 4; i++)
-        x->quat_vec[i] = sp[1]->s_vec + sp[1]->s_n * (i % quat_channels);
-    
+    // Assign input channels or zero buffer
+    for (int i = 0; i < 3; i++) {
+        if (i < vec_channels) {
+            x->vec_in[i] = sp[0]->s_vec + vec_size * i;
+        } else {
+            x->vec_in[i] = x->zero_buffer;
+        }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        if (i < quat_channels) {
+            x->quat_in[i] = sp[1]->s_vec + vec_size * i;
+        } else {
+            x->quat_in[i] = x->zero_buffer;
+        }
+    }
+
     signal_setmultiout(&sp[2], 3);
     for (int i = 0; i < 3; i++)
-        x->out_vec[i] = sp[2]->s_vec + sp[2]->s_n * i;
+        x->vec_out[i] = sp[2]->s_vec + sp[2]->s_n * i;
 
     dsp_add(qvrot_tilde_perform, 2, x, sp[0]->s_n);
 }
@@ -76,8 +104,8 @@ void *qvrot_tilde_new(void)
 {
     t_qvrot_tilde *x = (t_qvrot_tilde *)pd_new(qvrot_tilde_class);
     x->vec_in = (t_sample **)getbytes(3 * sizeof(t_sample *));
-    x->quat_vec = (t_sample **)getbytes(4 * sizeof(t_sample *));
-    x->out_vec = (t_sample **)getbytes(3 * sizeof(t_sample *));
+    x->quat_in = (t_sample **)getbytes(4 * sizeof(t_sample *));
+    x->vec_out = (t_sample **)getbytes(3 * sizeof(t_sample *));
     inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
     outlet_new(&x->x_obj, &s_signal);
     return (void *)x;
@@ -86,8 +114,11 @@ void *qvrot_tilde_new(void)
 void qvrot_tilde_free(t_qvrot_tilde *x)
 {
     freebytes(x->vec_in, 3 * sizeof(t_sample *));
-    freebytes(x->quat_vec, 4 * sizeof(t_sample *));
-    freebytes(x->out_vec, 3 * sizeof(t_sample *));
+    freebytes(x->quat_in, 4 * sizeof(t_sample *));
+    freebytes(x->vec_out, 3 * sizeof(t_sample *));
+    if (x->zero_buffer) {
+        freebytes(x->zero_buffer, x->buffer_size * sizeof(t_sample));
+    }
 }
 
 void qvrot_tilde_setup(void)
