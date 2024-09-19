dc8472db067516a7930afd7ca8cbd0b7b0c83c2d
Ben Wesch <benjamin.wesch@gmail.com>
2024-09-19 15:26:35 +0200

    add kernel signal inlet - closes #1

--- mc_conv~.c
+++ mc_conv~.c
@@ -1,5 +1,6 @@
 #include "m_pd.h"
 #include <string.h>
+#include <math.h>
 #include <stdlib.h>
 
 static t_class *mc_conv_tilde_class;
@@ -9,31 +10,35 @@ typedef struct _mc_conv_tilde {
     t_sample f;
     t_sample **in;
     t_sample **out;
-    t_float *kernel;
-    int kernel_size;
+    t_sample **kernel_in;
+    t_float *msg_kernel;  // Message-defined kernel
+    int msg_kernel_size;  // Size of message-defined kernel
+    t_float *active_kernel;  // Pointer to the currently active kernel
+    int kernel_size;  // Size of the active kernel
+    int input_chans;
     int is_cyclic;     // 1 for cyclic, 0 for non-cyclic
     int is_normalized; // 1 for normalized, 0 for non-normalized
-    int n_chans;
     int vec_size;
     t_sample *in_buffer; // Preallocated input buffer
+    int use_signal_kernel; // Flag to indicate if signal kernel is being used
 } t_mc_conv_tilde;
 
-static void mc_conv_tilde_normalize_kernel(t_mc_conv_tilde *x)
+// Normalize the kernel
+static void mc_conv_tilde_normalize_kernel(t_float *kernel, int size)
 {
-    if (x->is_normalized && x->kernel) {
-        float sum = 0;
-        int i;
-        for (i = 0; i < x->kernel_size; i++) {
-            sum += x->kernel[i];
-        }
-        if (sum != 0) {
-            for (i = 0; i < x->kernel_size; i++) {
-                x->kernel[i] /= sum;
-            }
+    float sum = 0;
+    int i;
+    for (i = 0; i < size; i++) {
+        sum += fabsf(kernel[i]);
+    }
+    if (sum != 0) {
+        for (i = 0; i < size; i++) {
+            kernel[i] /= sum;
         }
     }
 }
 
+// Perform the convolution
 static t_int *mc_conv_tilde_perform(t_int *w)
 {
     t_mc_conv_tilde *x = (t_mc_conv_tilde *)(w[1]);
@@ -41,35 +46,42 @@ static t_int *mc_conv_tilde_perform(t_int *w)
     int i, j, k;
     t_sample sum;
     int half_kernel = x->kernel_size / 2;
-
-    // If no kernel is set, just copy input to output
-    if (!x->kernel || x->kernel_size == 0) {
-        for (i = 0; i < x->n_chans; i++) {
-            for (j = 0; j < n; j++) {
-                x->out[i][j] = x->in[i][j];
-            }
-        }
-        return (w+3);
-    }
+    t_float normalized_kernel[x->kernel_size];
 
     for (i = 0; i < n; i++) {
         // Copy input to buffer
-        for (j = 0; j < x->n_chans; j++) {
+        for (j = 0; j < x->input_chans; j++) {
             x->in_buffer[j] = x->in[j][i];
         }
 
-        for (j = 0; j < x->n_chans; j++) {
+        // Use signal kernel if connected
+        if (x->use_signal_kernel) {
+            for (k = 0; k < x->kernel_size; k++) {
+                x->active_kernel[k] = x->kernel_in[k][i];
+            }
+        }
+
+        // Normalize kernel if required
+        t_float *current_kernel = x->active_kernel;
+        if (x->is_normalized) {
+            memcpy(normalized_kernel, x->active_kernel, x->kernel_size * sizeof(t_float));
+            mc_conv_tilde_normalize_kernel(normalized_kernel, x->kernel_size);
+            current_kernel = normalized_kernel;
+        }
+
+        // Perform convolution
+        for (j = 0; j < x->input_chans; j++) {
             sum = 0;
             for (k = 0; k < x->kernel_size; k++) {
                 int idx = j - half_kernel + k;
                 if (x->is_cyclic) {
-                    idx = (idx + x->n_chans) % x->n_chans;
-                    if (idx < 0) idx += x->n_chans;
+                    idx = (idx + x->input_chans) % x->input_chans;
+                    if (idx < 0) idx += x->input_chans;
                 } else {
                     if (idx < 0) idx = 0;
-                    if (idx >= x->n_chans) idx = x->n_chans - 1;
+                    if (idx >= x->input_chans) idx = x->input_chans - 1;
                 }
-                sum += x->in_buffer[idx] * x->kernel[k];
+                sum += x->in_buffer[idx] * current_kernel[k];
             }
             x->out[j][i] = sum;
         }
@@ -82,73 +94,100 @@ static void mc_conv_tilde_dsp(t_mc_conv_tilde *x, t_signal **sp)
 {
     int i;
     int vec_size = sp[0]->s_n;
-    int n_chans = sp[0]->s_nchans;
+    int input_chans = sp[0]->s_nchans;
+    int kernel_chans = sp[1]->s_nchans;
 
-    // Free previous memory if it exists
-    if (x->in) freebytes(x->in, x->n_chans * sizeof(t_sample *));
-    if (x->out) freebytes(x->out, x->n_chans * sizeof(t_sample *));
-    if (x->in_buffer) freebytes(x->in_buffer, x->n_chans * sizeof(t_sample));
+    // Free previous memory
+    if (x->in) freebytes(x->in, x->input_chans * sizeof(t_sample *));
+    if (x->out) freebytes(x->out, x->input_chans * sizeof(t_sample *));
+    if (x->in_buffer) freebytes(x->in_buffer, x->input_chans * sizeof(t_sample));
+    if (x->kernel_in) freebytes(x->kernel_in, x->kernel_size * sizeof(t_sample *));
     
-    x->n_chans = n_chans;
+    x->input_chans = input_chans;
     x->vec_size = vec_size;
 
-    // Allocate memory for input and output channel pointers
-    x->in = (t_sample **)getbytes(n_chans * sizeof(t_sample *));
-    x->out = (t_sample **)getbytes(n_chans * sizeof(t_sample *));
-    x->in_buffer = (t_sample *)getbytes(n_chans * sizeof(t_sample));
+    // Determine if we're using a signal kernel (more than 1 channel or non-zero single channel)
+    x->use_signal_kernel = (kernel_chans > 1) || (kernel_chans == 1 && sp[1]->s_vec[0] != 0);
+    
+    if (x->use_signal_kernel) {
+        x->kernel_size = kernel_chans;
+        x->active_kernel = (t_float *)getbytes(x->kernel_size * sizeof(t_float));
+    } else {
+        x->kernel_size = x->msg_kernel_size;
+        x->active_kernel = x->msg_kernel;
+    }
 
-    if (!x->in || !x->out || !x->in_buffer) {
+    // Allocate memory
+    x->in = (t_sample **)getbytes(input_chans * sizeof(t_sample *));
+    x->out = (t_sample **)getbytes(input_chans * sizeof(t_sample *));
+    x->in_buffer = (t_sample *)getbytes(input_chans * sizeof(t_sample));
+    x->kernel_in = (t_sample **)getbytes(x->kernel_size * sizeof(t_sample *));
+
+    if (!x->in || !x->out || !x->in_buffer || !x->kernel_in || (x->use_signal_kernel && !x->active_kernel)) {
         pd_error(x, "mc_conv~: out of memory");
         return;
     }
 
     // Assign input channels
-    for (i = 0; i < n_chans; i++) {
+    for (i = 0; i < input_chans; i++) {
         x->in[i] = sp[0]->s_vec + vec_size * i;
     }
 
+    // Assign kernel input channels
+    for (i = 0; i < x->kernel_size; i++) {
+        x->kernel_in[i] = (i < kernel_chans) ? sp[1]->s_vec + vec_size * i : sp[1]->s_vec;
+    }
+
     // Assign output channels
-    signal_setmultiout(&sp[1], n_chans);
-    for (i = 0; i < n_chans; i++) {
-        x->out[i] = sp[1]->s_vec + sp[1]->s_n * i;
+    signal_setmultiout(&sp[2], input_chans);
+    for (i = 0; i < input_chans; i++) {
+        x->out[i] = sp[2]->s_vec + sp[2]->s_n * i;
     }
 
     dsp_add(mc_conv_tilde_perform, 2, x, sp[0]->s_n);
 }
 
+// Set the kernel via message
 static void mc_conv_tilde_set_kernel(t_mc_conv_tilde *x, t_symbol *s, int argc, t_atom *argv)
 {
     (void)s; // Unused parameter
 
-    int i;
-
-    if (argc % 2 == 0) {
-        pd_error(x, "mc_conv~: kernel must have an odd number of elements");
+    if (argc == 0) {
+        pd_error(x, "mc_conv~: kernel must have at least one element");
         return;
     }
 
-    // Free the old kernel if it exists
-    if (x->kernel) {
-        freebytes(x->kernel, x->kernel_size * sizeof(t_float));
+    // Ensure odd kernel size
+    int new_kernel_size = argc;
+    if (new_kernel_size % 2 == 0) {
+        new_kernel_size++;
+        post("mc_conv~: kernel size must be odd, appending 0");
     }
 
-    // Allocate memory for the new kernel
-    x->kernel_size = argc;
-    x->kernel = (t_float *)getbytes(x->kernel_size * sizeof(t_float));
-
-    if (!x->kernel) {
-        pd_error(x, "mc_conv~: out of memory");
-        x->kernel_size = 0;
-        return;
+    // Reallocate msg_kernel if size changed
+    if (new_kernel_size != x->msg_kernel_size) {
+        x->msg_kernel = (t_float *)resizebytes(x->msg_kernel, x->msg_kernel_size * sizeof(t_float), new_kernel_size * sizeof(t_float));
+        if (!x->msg_kernel) {
+            pd_error(x, "mc_conv~: out of memory");
+            x->msg_kernel_size = 0;
+            return;
+        }
+        x->msg_kernel_size = new_kernel_size;
     }
 
     // Set the new kernel values
-    for (i = 0; i < x->kernel_size; i++) {
-        x->kernel[i] = atom_getfloat(argv + i);
+    for (int i = 0; i < argc; i++) {
+        x->msg_kernel[i] = atom_getfloat(argv + i);
+    }
+    if (new_kernel_size > argc) {
+        x->msg_kernel[argc] = 0; // Set the added element to 0 if we padded
     }
 
-    // Normalize if required
-    mc_conv_tilde_normalize_kernel(x);
+    // If not using signal kernel, update active kernel
+    if (!x->use_signal_kernel) {
+        x->active_kernel = x->msg_kernel;
+        x->kernel_size = x->msg_kernel_size;
+    }
 }
 
 static void mc_conv_tilde_set_cyclic(t_mc_conv_tilde *x, t_floatarg f)
@@ -159,7 +198,6 @@ static void mc_conv_tilde_set_cyclic(t_mc_conv_tilde *x, t_floatarg f)
 static void mc_conv_tilde_set_normalize(t_mc_conv_tilde *x, t_floatarg f)
 {
     x->is_normalized = (f != 0);
-    mc_conv_tilde_normalize_kernel(x);
 }
 
 static void *mc_conv_tilde_new(t_symbol *s, int argc, t_atom *argv)
@@ -168,25 +206,37 @@ static void *mc_conv_tilde_new(t_symbol *s, int argc, t_atom *argv)
 
     t_mc_conv_tilde *x = (t_mc_conv_tilde *)pd_new(mc_conv_tilde_class);
 
-    x->kernel = NULL;
+    x->msg_kernel = NULL;
+    x->msg_kernel_size = 0;
+    x->active_kernel = NULL;
     x->kernel_size = 0;
     x->is_cyclic = 0;
     x->is_normalized = 0;
     x->in = NULL;
     x->out = NULL;
-    x->n_chans = 0;
+    x->kernel_in = NULL;
+    x->input_chans = 0;
     x->vec_size = 0;
     x->in_buffer = NULL;
+    x->use_signal_kernel = 0;
 
     // Parse creation arguments
     while (argc && argv->a_type == A_SYMBOL) {
         if (atom_getsymbol(argv) == gensym("-c")) x->is_cyclic = 1;
         else if (atom_getsymbol(argv) == gensym("-n")) x->is_normalized = 1;
-        else pd_error(x, "mc_conv~: invalid argument");
+        else pd_error(x, "mc_conv~: invalid flags");
         argc--, argv++;
     }
-    if (argc) mc_conv_tilde_set_kernel(x, &s_, argc, argv); // read kernel from args if present
+    if (argc) {
+        mc_conv_tilde_set_kernel(x, &s_, argc, argv); // read kernel from args if present
+    } else {
+        // If no kernel is provided, initialize with a default kernel
+        t_atom default_kernel[1];
+        SETFLOAT(default_kernel, 1.0f);
+        mc_conv_tilde_set_kernel(x, &s_, 1, default_kernel);
+    }
 
+    inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal); // Add second inlet for kernel
     outlet_new(&x->x_obj, &s_signal);
 
     return (void *)x;
@@ -194,18 +244,12 @@ static void *mc_conv_tilde_new(t_symbol *s, int argc, t_atom *argv)
 
 static void mc_conv_tilde_free(t_mc_conv_tilde *x)
 {
-    if (x->kernel) {
-        freebytes(x->kernel, x->kernel_size * sizeof(t_float));
-    }
-    if (x->in) {
-        freebytes(x->in, x->n_chans * sizeof(t_sample *));
-    }
-    if (x->out) {
-        freebytes(x->out, x->n_chans * sizeof(t_sample *));
-    }
-    if (x->in_buffer) {
-        freebytes(x->in_buffer, x->n_chans * sizeof(t_sample));
-    }
+    if (x->in) freebytes(x->in, x->input_chans * sizeof(t_sample *));
+    if (x->out) freebytes(x->out, x->input_chans * sizeof(t_sample *));
+    if (x->in_buffer) freebytes(x->in_buffer, x->input_chans * sizeof(t_sample));
+    if (x->msg_kernel) freebytes(x->msg_kernel, x->msg_kernel_size * sizeof(t_float));
+    if (x->kernel_in) freebytes(x->kernel_in, x->kernel_size * sizeof(t_sample *));
+    if (x->use_signal_kernel && x->active_kernel) freebytes(x->active_kernel, x->kernel_size * sizeof(t_float));
 }
 
 void mc_conv_tilde_setup(void)
