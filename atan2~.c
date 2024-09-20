d626ce6de3cf0962ed6c780deede547f3f6acbc2
Ben Wesch <benjamin.wesch@gmail.com>
2024-09-20 13:32:28 +0200

    add atan2 helpfile

--- atan2~.c
+++ atan2~.c
@@ -1,16 +1,14 @@
 /*
 atan2~ - Signal rate atan2 external for Pure Data
-
 2024, Assistant
-
 Functionality:
 - Takes two signal inlets for y and x
 - Outputs the atan2 of these values as a signal
-
+- Optional 'turn' argument to output in range 0..1 instead of 0..2π
 Usage:
-1. Connect signal inputs to the left (y) and right (x) inlets
-2. Use the outlet for the atan2 result
-
+1. Create object: [atan2~] or [atan2~ turn]
+2. Connect signal inputs to the left (y) and right (x) inlets
+3. Use the outlet for the atan2 result
 Note: This code was developed with assistance from an AI language model.
 */
 
@@ -24,6 +22,7 @@ typedef struct _atan2_tilde {
     t_sample f_dummy;
     t_inlet *x_in2;    // Inlet for the x value
     t_outlet *x_out;
+    int x_turn;        // Flag for turn mode (0..1 range)
 } t_atan2_tilde;
 
 static t_int *atan2_tilde_perform(t_int *w) {
@@ -33,8 +32,18 @@ static t_int *atan2_tilde_perform(t_int *w) {
     t_sample *out = (t_sample *)(w[4]);
     int n = (int)(w[5]);
     
-    while (n--) {
-        *out++ = atan2f(*in1++, *in2++);
+    if (x->x_turn) {
+        // Output in range 0..1
+        while (n--) {
+            float angle = atan2f(*in1++, *in2++);
+            if (angle < 0) angle += 2 * M_PI;
+            *out++ = angle / (2 * M_PI);
+        }
+    } else {
+        // Standard output in radians
+        while (n--) {
+            *out++ = atan2f(*in1++, *in2++);
+        }
     }
     
     return (w+6);
@@ -45,10 +54,22 @@ static void atan2_tilde_dsp(t_atan2_tilde *x, t_signal **sp) {
             sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[0]->s_n);
 }
 
-static void *atan2_tilde_new(void) {
+static void *atan2_tilde_new(t_symbol *s, int argc, t_atom *argv) {
+    (void)s;
+    
     t_atan2_tilde *x = (t_atan2_tilde *)pd_new(atan2_tilde_class);
     x->x_in2 = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
     x->x_out = outlet_new(&x->x_obj, &s_signal);
+    
+    x->x_turn = 0;  // Default to standard radian output
+    
+    // Check for 'turn' argument
+    if (argc > 0 && argv[0].a_type == A_SYMBOL) {
+        if (atom_getsymbol(&argv[0]) == gensym("turn")) {
+            x->x_turn = 1;
+        }
+    }
+    
     return (void *)x;
 }
 
@@ -57,7 +78,7 @@ void atan2_tilde_setup(void) {
         (t_newmethod)atan2_tilde_new,
         0, sizeof(t_atan2_tilde),
         CLASS_DEFAULT,
-        0);
+        A_GIMME, 0);
     
     class_addmethod(atan2_tilde_class, (t_method)atan2_tilde_dsp, gensym("dsp"), A_CANT, 0);
     CLASS_MAINSIGNALIN(atan2_tilde_class, t_atan2_tilde, f_dummy);
