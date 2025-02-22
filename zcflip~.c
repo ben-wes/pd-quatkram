4a1bddb137982cf0b681271d575eb698ef754573
Ben Wesch <benjamin.wesch@gmail.com>
2025-02-22 10:27:48 +0100

    change mechanism to continuously track inputs' zc and allow rapid switching

--- zcflip~.c
+++ zcflip~.c
@@ -14,18 +14,18 @@ typedef struct _zcflip_tilde {
     int write_pos1;
     
     // State tracking
-    int current_in;          // which input is active (0 or 1)
+    int active_chan;         // which input is active (0 or 1)
     int switch_pending;      // flag for pending switch
-    int silent_zc_found;     // flag for found zero crossing in silent signal
     int delay_samples;       // current delay in samples
-    int stored_delay;        // stored delay for silent ZC detection
+    int stored_delay0;       // stored delay for channel 0
+    int stored_delay1;       // stored delay for channel 1
     
     // Flag for delay outlets
     int has_delay_outs;
     
     t_outlet *out0;
     t_outlet *out1;
-    t_outlet *active_chan;
+    t_outlet *active_chan_out;
     t_outlet *delay_out0;
     t_outlet *delay_out1;
 } t_zcflip_tilde;
@@ -43,63 +43,60 @@ static t_int *zcflip_tilde_perform(t_int *w) {
     int n = (int)(w[x->has_delay_outs ? 10 : 8]);
     
     for (int i = 0; i < n; i++) {
-        if (trig[i] > 0 && !x->switch_pending) {
-            x->switch_pending = 1;
-            x->silent_zc_found = 0;
-            x->stored_delay = 0;
-        }
-        
+        // Store current samples
         x->buffer0[x->write_pos0] = in0[i];
         x->buffer1[x->write_pos1] = in1[i];
         
-        int silent_buffer = !x->current_in;
-        t_sample silent_prev = (silent_buffer == 0) ? 
-            x->buffer0[(x->write_pos0 - 1 + x->buffer_size) % x->buffer_size] :
-            x->buffer1[(x->write_pos1 - 1 + x->buffer_size) % x->buffer_size];
-        t_sample silent_curr = (silent_buffer == 0) ? in0[i] : in1[i];
+        // Track zero-crossings for both channels
+        t_sample prev0 = x->buffer0[(x->write_pos0 - 1 + x->buffer_size) % x->buffer_size];
+        t_sample prev1 = x->buffer1[(x->write_pos1 - 1 + x->buffer_size) % x->buffer_size];
         
-        if (!x->silent_zc_found && silent_prev <= 0 && silent_curr > 0) {
-            x->silent_zc_found = 1;
-            x->stored_delay = -1;
-        }
+        // Check for zero-crossings and reset respective delays
+        if (prev0 <= 0 && in0[i] > 0) x->stored_delay0 = 0;
+        if (prev1 <= 0 && in1[i] > 0) x->stored_delay1 = 0;
         
-        if (x->silent_zc_found && x->stored_delay < x->buffer_size) {
-            x->stored_delay++;
-        }
+        // Increment delays for both channels
+        if (x->stored_delay0 >= 0) x->stored_delay0++;
+        if (x->stored_delay1 >= 0) x->stored_delay1++;
         
-        if (x->switch_pending && x->silent_zc_found) {
-            int active_read_pos = (x->current_in == 0) ? 
+        // Handle switching
+        if (trig[i] > 0 && !x->switch_pending) x->switch_pending = 1;
+        
+        // Check for zero-crossing in active channel to complete switch
+        if (x->switch_pending) {
+            int active_read_pos = (x->active_chan == 0) ? 
                 x->write_pos0 : x->write_pos1;
             active_read_pos -= x->delay_samples;
             if (active_read_pos < 0) active_read_pos += x->buffer_size;
             
-            t_sample active_prev = (x->current_in == 0) ? 
+            t_sample active_prev = (x->active_chan == 0) ? 
                 x->buffer0[(active_read_pos - 1 + x->buffer_size) % x->buffer_size] :
                 x->buffer1[(active_read_pos - 1 + x->buffer_size) % x->buffer_size];
-            t_sample active_curr = (x->current_in == 0) ? 
+            t_sample active_curr = (x->active_chan == 0) ? 
                 x->buffer0[active_read_pos] :
                 x->buffer1[active_read_pos];
             
             if (active_prev <= 0 && active_curr > 0) {
-                x->current_in = !x->current_in;
+                x->active_chan = !x->active_chan;
+                // Use the stored delay of the channel we're switching to
+                x->delay_samples = (x->active_chan ? x->stored_delay1 : x->stored_delay0) - 1;
                 x->switch_pending = 0;
-                x->delay_samples = x->stored_delay;
             }
         }
         
+        // Output using current delay
         int read_pos0 = x->write_pos0 - x->delay_samples;
         int read_pos1 = x->write_pos1 - x->delay_samples;
         if (read_pos0 < 0) read_pos0 += x->buffer_size;
         if (read_pos1 < 0) read_pos1 += x->buffer_size;
         
-        active[i] = x->current_in;
-        out0[i] = x->current_in ? 0 : x->buffer0[read_pos0];
-        out1[i] = x->current_in ? x->buffer1[read_pos1] : 0;
+        active[i] = x->active_chan;
+        out0[i] = x->active_chan ? 0 : x->buffer0[read_pos0];
+        out1[i] = x->active_chan ? x->buffer1[read_pos1] : 0;
         
-        // Output delays if enabled
         if (x->has_delay_outs) {
-            delay0[i] = x->current_in ? 0 : x->delay_samples;
-            delay1[i] = x->current_in ? x->delay_samples : 0;
+            delay0[i] = x->active_chan ? 0 : x->delay_samples;
+            delay1[i] = x->active_chan ? x->delay_samples : 0;
         }
         
         x->write_pos0 = (x->write_pos0 + 1) % x->buffer_size;
@@ -107,6 +104,19 @@ static t_int *zcflip_tilde_perform(t_int *w) {
     }
     
     return (w + (x->has_delay_outs ? 11 : 9));
+}
+
+static void zcflip_tilde_dsp(t_zcflip_tilde *x, t_signal **sp) {
+    if (x->has_delay_outs) {
+        dsp_add(zcflip_tilde_perform, 10,
+                x, sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec,
+                sp[3]->s_vec, sp[4]->s_vec, sp[5]->s_vec,
+                sp[6]->s_vec, sp[7]->s_vec, sp[0]->s_n);
+    } else {
+        dsp_add(zcflip_tilde_perform, 8,
+                x, sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec,
+                sp[3]->s_vec, sp[4]->s_vec, sp[5]->s_vec, sp[0]->s_n);
+    }
 }
 
 static void *zcflip_tilde_new(t_symbol *s, int argc, t_atom *argv) {
@@ -135,10 +145,11 @@ static void *zcflip_tilde_new(t_symbol *s, int argc, t_atom *argv) {
     
     x->write_pos0 = 0;
     x->write_pos1 = 0;
-    x->current_in = 0;
+    x->active_chan = 0;
     x->switch_pending = 0;
-    x->silent_zc_found = 0;
     x->delay_samples = 0;
+    x->stored_delay0 = -1;
+    x->stored_delay1 = -1;
     
     for (int i = 0; i < x->buffer_size; i++) {
         x->buffer0[i] = 0;
@@ -149,7 +160,7 @@ static void *zcflip_tilde_new(t_symbol *s, int argc, t_atom *argv) {
     inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
     x->out0 = outlet_new(&x->x_obj, &s_signal);
     x->out1 = outlet_new(&x->x_obj, &s_signal);
-    x->active_chan = outlet_new(&x->x_obj, &s_signal);
+    x->active_chan_out = outlet_new(&x->x_obj, &s_signal);
     
     // Create delay outlets if flag is set
     if (x->has_delay_outs) {
@@ -165,23 +176,10 @@ static void zcflip_tilde_free(t_zcflip_tilde *x) {
     if (x->buffer1) freebytes(x->buffer1, x->buffer_size * sizeof(t_sample));
     outlet_free(x->out0);
     outlet_free(x->out1);
-    outlet_free(x->active_chan);
+    outlet_free(x->active_chan_out);
     if (x->has_delay_outs) {
         outlet_free(x->delay_out0);
         outlet_free(x->delay_out1);
-    }
-}
-
-static void zcflip_tilde_dsp(t_zcflip_tilde *x, t_signal **sp) {
-    if (x->has_delay_outs) {
-        dsp_add(zcflip_tilde_perform, 10,
-                x, sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec,
-                sp[3]->s_vec, sp[4]->s_vec, sp[5]->s_vec,
-                sp[6]->s_vec, sp[7]->s_vec, sp[0]->s_n);
-    } else {
-        dsp_add(zcflip_tilde_perform, 8,
-                x, sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec,
-                sp[3]->s_vec, sp[4]->s_vec, sp[5]->s_vec, sp[0]->s_n);
     }
 }
 
