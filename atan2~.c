9050f6db525528dfc0a7ba7facc6ff7fbd28a673
Ben Wesch <benjamin.wesch@gmail.com>
2025-04-12 10:57:14 +0200

    atan2~: add multichannel support

--- atan2~.c
+++ atan2~.c
@@ -5,6 +5,8 @@ Functionality:
 - Takes two signal inlets for y and x
 - Outputs the atan2 of these values as a signal
 - Optional 'turn' argument to output in range 0..1 instead of 0..2π
+- Supports multichannel input/output
+- Left inlet (y) determines channel count, right inlet (x) is wrapped if needed
 Usage:
 1. Create object: [atan2~] or [atan2~ turn]
 2. Connect signal inputs to the left (y) and right (x) inlets
@@ -20,30 +22,45 @@ static t_class *atan2_tilde_class;
 typedef struct _atan2_tilde {
     t_object x_obj;
     t_sample f_dummy;
-    t_inlet *x_in2;    // Inlet for the x value
-    t_outlet *x_out;
-    float scale;       // 1/2π for turns, 1.0 for radians, 180/π for degrees
+    t_inlet *x_in2;          // Inlet for the x value
+    float scale;             // 1/2π for turns, 1.0 for radians, 180/π for degrees
 } t_atan2_tilde;
 
 static t_int *atan2_tilde_perform(t_int *w) {
     t_atan2_tilde *x = (t_atan2_tilde *)(w[1]);
-    t_sample *in1 = (t_sample *)(w[2]);  // y input
-    t_sample *in2 = (t_sample *)(w[3]);  // x input
+    t_sample *y_in = (t_sample *)(w[2]);  // y input
+    t_sample *x_in = (t_sample *)(w[3]);  // x input
     t_sample *out = (t_sample *)(w[4]);
     int n = (int)(w[5]);
+    int nchans = (int)(w[6]);
+    int x_nchans = (int)(w[7]);
     
     while (n--) {
-        float angle = atan2f(*in1++, *in2++);
-        if (angle < 0) angle += 2 * M_PI;
-        *out++ = angle * x->scale;
+        for (int ch = 0; ch < nchans; ch++) {
+            // Wrap the x input channel if needed
+            int x_ch = ch % x_nchans;
+            float angle = atan2f(y_in[ch], x_in[x_ch]);
+            if (angle < 0) angle += 2 * M_PI;
+            out[ch] = angle * x->scale;
+        }
+        y_in += nchans;
+        x_in += x_nchans;
+        out += nchans;
     }
     
-    return (w+6);
+    return (w+8);
 }
 
 static void atan2_tilde_dsp(t_atan2_tilde *x, t_signal **sp) {
-    dsp_add(atan2_tilde_perform, 5, x,
-            sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[0]->s_n);
+    int y_nchans = sp[0]->s_nchans;
+    int x_nchans = sp[1]->s_nchans;
+    
+    // Left inlet (y) determines output channel count
+    signal_setmultiout(&sp[2], y_nchans);
+    
+    dsp_add(atan2_tilde_perform, 7, x,
+            sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, 
+            sp[0]->s_n, y_nchans, x_nchans);
 }
 
 static void *atan2_tilde_new(t_symbol *s, int argc, t_atom *argv) {
@@ -51,7 +68,7 @@ static void *atan2_tilde_new(t_symbol *s, int argc, t_atom *argv) {
     
     t_atan2_tilde *x = (t_atan2_tilde *)pd_new(atan2_tilde_class);
     x->x_in2 = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
-    x->x_out = outlet_new(&x->x_obj, &s_signal);
+    outlet_new(&x->x_obj, gensym("signal"));
     
     x->scale = 0.5f / M_PI;  // Default to turns (0..1)
     
@@ -70,8 +87,9 @@ static void *atan2_tilde_new(t_symbol *s, int argc, t_atom *argv) {
 void atan2_tilde_setup(void) {
     atan2_tilde_class = class_new(gensym("atan2~"),
         (t_newmethod)atan2_tilde_new,
-        0, sizeof(t_atan2_tilde),
-        CLASS_DEFAULT,
+        0, 
+        sizeof(t_atan2_tilde),
+        CLASS_MULTICHANNEL,
         A_GIMME, 0);
     
     class_addmethod(atan2_tilde_class, (t_method)atan2_tilde_dsp, gensym("dsp"), A_CANT, 0);
